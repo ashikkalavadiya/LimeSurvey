23b110353c1f6ab6a6b16336015e71562e0810d3
Chintan Mirani <chintan.mirani@gmail.com>
2018-08-14 11:25:20 +0530

    Make api call to worddiagnostic to save survey response in worddignostic site.

---
+++ plugins/ShowResponse/ShowResponse.php
@@ -0,0 +1,39 @@
+<?php
+class ShowResponse extends PluginBase {
+    protected $storage = 'DbStorage';    
+    static protected $description = 'Demo: handle a survey response';
+    static protected $name = 'Show response';
+    
+    public function init()
+    {
+        /**
+         * Here you should handle subscribing to the events your plugin will handle
+         */
+        $this->subscribe('afterSurveyComplete', 'showTheResponse');
+    }
+    
+    /*
+     * Below are the actual methods that handle events
+     */
+    public function showTheResponse() 
+    {
+        $event      = $this->getEvent();        
+        $surveyId   = $event->get('surveyId');
+        $responseId = $event->get('responseId');
+        $response   = $this->pluginManager->getAPI()->getResponse($surveyId, $responseId);
+
+        $data = [
+            'survey_id' => $surveyId,
+            'response_id' => $responseId,
+            'response_data' => json_encode($response)
+        ];
+        
+        if (Yii::app()->user) {
+            $users = User::model()->findByPk(Yii::app()->user->id);
+            $data['user_id'] = Yii::app()->user->id;
+            $data['user_name'] = $users->full_name; 
+        }
+        $output = Yii::app()->curl->post('http://worddiagnostics.com/limesurvey-api/add-response', $data);
+        $event->getContent($this);
+    }    
+}
