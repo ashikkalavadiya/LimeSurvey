95d24883c9f06a67c96c4c7a58e3add25366a9b7
Chintan Mirani <chintan.mirani@gmail.com>
2020-01-02 12:08:02 +0100

    Move cursor to next input box when enter space in input box

--- themes/survey/vanilla/scripts/custom.js
+++ themes/survey/vanilla/scripts/custom.js
@@ -8,8 +8,17 @@
 
 
 $(document).on('ready pjax:scriptcomplete',function(){
-    /**
+    /** 
      * Code included inside this will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
      * @see https://learn.jquery.com/using-jquery-core/document-ready/
      */
+
+    $(".multiple-short-txt").bind("keypress", function (e) {
+        if (e.key === ' ' || e.key === 'Spacebar') {
+            // ' ' is standard, 'Spacebar' was used by IE9 and Firefox < 37
+            e.preventDefault();
+            $('#' + e.target.id).closest('li').next('li').find('input:text').focus();
+        }
+    });
 });
+
